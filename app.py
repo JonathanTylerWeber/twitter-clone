5e031194a7947fe6732cb3046033236ff9f0dadc
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-12-23 18:09:59 +0800

    all routes done

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
+import pdb
 
 CURR_USER_KEY = "curr_user"
 
@@ -18,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -113,7 +114,10 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    if g.user:
+            do_logout()
+            flash("You logged out", "success")
+            return redirect("/")
 
 
 ##############################################################################
@@ -141,6 +145,8 @@ def users_show(user_id):
     """Show user profile."""
 
     user = User.query.get_or_404(user_id)
+
+    # pdb.set_trace()
 
     # snagging messages in order from the database;
     # user.messages won't be in order by default
@@ -211,8 +217,22 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
-
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    user = g.user
+    form = UserEditForm(obj=user)
+    if form.validate_on_submit():
+        if User.authenticate(form.username.data, form.password.data):
+            form.populate_obj(user)
+            db.session.commit()
+            flash("User info updated!", "success")
+            return redirect(f"/users/{user.id}")
+        else:
+            flash("Incorrect Password.", 'danger')
+            return redirect('/')
+    return render_template('users/edit.html', form=form, user=user)
+        
 
 @app.route('/users/delete', methods=["POST"])
 def delete_user():
@@ -292,8 +312,18 @@ def homepage():
     """
 
     if g.user:
+        followed_users = [user.id for user in g.user.following]
+
+        own_messages = (Message
+                        .query
+                        .filter(Message.user_id == g.user.id)
+                        .order_by(Message.timestamp.desc())
+                        .limit(100)
+                        .all())
+
         messages = (Message
                     .query
+                    .filter((Message.user_id == g.user.id) | (Message.user_id.in_(followed_users)))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
