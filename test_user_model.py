a5572d91eab2db9b663691c8d8f9989aeb022805
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-12-28 19:55:12 +0800

    added some tests

--- test_user_model.py
+++ test_user_model.py
@@ -9,6 +9,9 @@ import os
 from unittest import TestCase
 
 from models import db, User, Message, Follows
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
+
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -56,3 +59,114 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_repr(self):
+        user = User(username='testuser', email='test@example.com')
+        expected_repr = f"<User #{user.id}: {user.username}, {user.email}>"
+        self.assertEqual(repr(user), expected_repr)
+
+    def test_is_following(self):
+        u1 = User(username='testuser1', email='test1@example.com')
+        u2 = User(username='testuser2', email='test2@example.com')
+
+        u1.following.append(u2)
+
+        self.assertTrue(u1.is_following(u2))
+        self.assertFalse(u2.is_following(u1))
+
+    def test_is_followed_by(self):
+        u1 = User(username='testuser1', email='test1@example.com')
+        u2 = User(username='testuser2', email='test2@example.com')
+
+        u1.following.append(u2)
+        u2.followers.append(u1)
+
+        self.assertTrue(u2.is_followed_by(u1))
+
+    def test_signup_with_valid_credentials(self):
+        # Provide valid credentials for the new user
+        username = 'testuser'
+        email = 'testuser@example.com'
+        password = 'password123'
+
+        # Call the User.signup() method to create a new user
+        new_user = User.signup(username, email, password, None)
+        db.session.commit()
+
+        # Assert that the new user is not None, indicating successful signup
+        self.assertIsNotNone(new_user)
+
+        # Assert that the new user's username and email match the provided credentials
+        self.assertEqual(new_user.username, username)
+        self.assertEqual(new_user.email, email)
+
+        # Get an instance of Bcrypt
+        bcrypt = Bcrypt()
+
+        # Assert that the new user's password is properly hashed and not equal to the original password
+        self.assertNotEqual(new_user.password, password)
+        self.assertTrue(bcrypt.check_password_hash(new_user.password, password))
+
+    FIXME:
+    def test_signup_with_invalid_data(self):
+        # Create a user with valid data
+        username = 'testuser'
+        email = 'testuser@example.com'
+        password = 'password123'
+        User.signup(username, email, password, None)
+        db.session.commit()
+
+        # Try to create a new user with the same username
+        with self.assertRaises(IntegrityError):
+            User.signup(username, 'duplicate@example.com', 'password456', None)
+            db.session.commit()
+
+        # Try to create a new user with the same email
+        with self.assertRaises(IntegrityError):
+            User.signup('duplicateuser', email, 'password789', None)
+            db.session.commit()
+
+        # Try to create a new user with missing required fields
+        with self.assertRaises(ValueError):
+            User.signup(None, 'missing@example.com', None, None)
+            db.session.commit()
+
+    def test_authenticate_with_valid_credentials(self):
+        # Create a new user with valid credentials
+        username = 'testuser'
+        email = 'testuser@example.com'
+        password = 'password123'
+        user = User.signup(username, email, password, None)
+        db.session.commit()
+
+        # Call the User.authenticate() method with the valid credentials
+        authenticated_user = User.authenticate(username, password)
+
+        # Assert that the authenticated user is not False, indicating successful authentication
+        self.assertNotEqual(authenticated_user, False)
+
+        # Assert that the authenticated user is the same as the original user
+        self.assertEqual(authenticated_user.username, user.username)
+
+    def test_authenticate_with_invalid_username(self):
+        # Call the User.authenticate() method with an invalid username
+        authenticated_user = User.authenticate('invalidusername', 'password123')
+
+        # Assert that the authenticated user is False, indicating failed authentication
+        self.assertEqual(authenticated_user, False)
+
+    def test_authenticate_with_invalid_password(self):
+        # Create a new user with valid credentials
+        username = 'testuser'
+        email = 'testuser@example.com'
+        password = 'password123'
+        User.signup(username, email, password, None)
+        db.session.commit()
+
+        # Call the User.authenticate() method with an invalid password
+        authenticated_user = User.authenticate(username, 'invalidpassword')
+
+        # Assert that the authenticated user is False, indicating failed authentication
+        self.assertEqual(authenticated_user, False)
+
+        
